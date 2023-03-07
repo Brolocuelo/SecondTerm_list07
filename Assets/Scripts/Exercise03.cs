2ac5d460ffbb368dff1aafaf3203f7fcfb2d945c
Brolocuelo <luisalfonsorello@gmail.com>
2023-03-07 13:16:43 +0100

    Comienzo ejercicio 3

--- Assets/Scripts/Exercise03.cs
+++ Assets/Scripts/Exercise03.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class Exercise03 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int value;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+
     }
 }
